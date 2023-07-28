3bf4898b8384f565041420ff39e8b6727dffce33
Jacobs <sjacobs@wpi.edu>
2023-07-28 09:02:25 -0400

    added first draft clickgame

---
+++ clickgame/game.js
@@ -0,0 +1,20 @@
+function buy(store) {
+    let cost;
+    let bank;
+
+}
+
+function harvest(widget) {
+
+    setTimeout(() => {
+
+    }, 1000);
+}
+
+function changeScore(amount) {
+
+}
+
+function showPoint(widget) {
+
+}
